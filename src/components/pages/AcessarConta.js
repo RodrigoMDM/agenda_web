954bb0531666bc20bf24758b945dba59a9cb3ae5
RodrigoMDM <rodrigo_md_marques@hotmail.com>
2022-01-15 08:56:57 -0300

    Aula REACT COTI 15/01/22

---
+++ src/components/pages/AcessarConta.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function AcessarConta() {
+    return (
+        <div className='container mt-3'>
+            <h1>
+                Acessar Conta!
+            </h1>
+        </div>
+    )
+}
---
+++ src/components/pages/CrieSuaConta.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function CrieSuaConta() {
+    return (
+        <div className='container mt-3'>
+            <h1>
+                Crie sua conta!
+            </h1>
+        </div>
+    )
+}
